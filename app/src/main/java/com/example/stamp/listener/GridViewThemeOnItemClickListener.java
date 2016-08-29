354126a98cdd837988dd3d52ecee6dfecdddd3a7
chenxr@ichinasoft.com <ichinasoft>
2016-08-29 16:08:29 +0800

    修改筛选条目监听，适配器

---
+++ app/src/main/java/com/example/stamp/listener/GridViewThemeOnItemClickListener.java
@@ -0,0 +1,45 @@
+package com.example.stamp.listener;
+
+import android.view.View;
+import android.widget.AdapterView;
+
+import com.example.stamp.adapter.StampTapFilterGridViewAdapter;
+import com.example.stamp.adapter.StampTapFilterThemeGridViewAdapter;
+
+/**
+ * GridViewThemeOnItemClickListener
+ * 邮票目录筛选题材的条目点击监听
+ * Created by Administrator on 2016/8/2.
+ */
+public class GridViewThemeOnItemClickListener implements AdapterView.OnItemClickListener {
+    private int position;
+    private StampTapFilterThemeGridViewAdapter adapter;
+
+    public GridViewThemeOnItemClickListener(int position, StampTapFilterThemeGridViewAdapter adapter) {
+        this.position = position;
+        this.adapter = adapter;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        if (position != i) {
+            position = i;
+            adapter.setSeclection(i);
+            adapter.notifyDataSetChanged();
+        } else {
+            position = -1;
+            adapter.setSeclection(adapter.getCount() + 1);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition() {
+        position = -1;
+        adapter.setSeclection(adapter.getCount() + 1);
+        adapter.notifyDataSetChanged();
+    }
+}
